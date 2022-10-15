e832206d9cdb212fa5e0ec8bd2a25f75a94e3b93
Sanjeev Dasgupta <sanjeev.dasgupta@gmail.com>
2022-10-15 11:24:38 +0530

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
+import theme from './theme';
+import { Navigate, BrowserRouter, Routes, Route} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import MyQuiz from './MyQuiz';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+
+
+
+const rootElement = document.getElementById('root')!;
+const root = createRoot(rootElement);
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+     <BrowserRouter>
+    <Routes> 
+      
+
+        <Route path="/" element={<MyQuiz />}/>
+        <Route path="/app" element={<App/>}/>
+        
+                
+      </Routes>
+     </BrowserRouter>
+  </ThemeProvider>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
